438fa49208fe6d21fcd66e0e2eb28d0a40c8b5fe
Baba Kane <bkane3850@gmail.com>
2025-08-27 11:58:49 +0000

    Préparer frontend pour déploiement

--- aminastore-frontend/src/config/api.js
+++ aminastore-frontend/src/config/api.js
@@ -1,3 +1,5 @@
+// src/config/api.js
+// Récupère l'URL de l'API depuis les variables d'environnement
 const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
 export const API_BASE = `${BASE_URL}`;
